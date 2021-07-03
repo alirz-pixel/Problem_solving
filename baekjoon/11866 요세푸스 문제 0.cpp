f61ae2669fa4559c1d71d03a95e0dd37c28ba3cc
alirz-pixel <alirce@naver.com>
2021-07-03 15:27:34 +0900

    백준 11866번 실버 4 (다시 풀음)

--- baekjoon/11866 요세푸스 문제 0.cpp
+++ baekjoon/11866 요세푸스 문제 0.cpp
@@ -1,5 +1,19 @@
+/*
+백준 11866번 요세푸스 문제 0 || 실버 4
+https://www.acmicpc.net/problem/11866
+
+
+데큐를 선언해, K칸 움직일 때까지 앞에서 pop하고 뒤에 push 해준다.
+ = (그냥 큐를 사용했어도 되었음)
+
+
+시작시간 : 3:12
+종료시간 : 3:21
+*/
+
+
 #include <iostream>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
@@ -9,35 +23,35 @@ int main(void)
     ios::sync_with_stdio(false);
     cout.tie(NULL);
 
-    int N, K, cnt = 1;
+
+    int cnt = 0;
+    int N, K;
     cin >> N >> K;
 
-    queue<int> josephus;
-
+    deque<int> DQ;
     for (int i = 1; i <= N; i++)
-        josephus.push(i);
+        DQ.push_back(i);
 
-
+    
     cout << "<";
-    while (josephus.front() != josephus.back())
+    while (!DQ.empty())
     {
-        if (cnt != K)
+        if (DQ.size() == 1)
+            break;
+
+        if (cnt == K - 1)
         {
-            josephus.push(josephus.front());
-            josephus.pop();
-            cnt++;
+            cnt = 0;
+            cout << DQ.front() << ", ";
+            DQ.pop_front();
+            continue;
         }
 
-        else // if (cnt == K)
-        {
-            cout << josephus.front() << ", ";
-            josephus.pop();
-
-            cnt = 1;
-        }
-    }
-
-    cout << josephus.front() << ">";
+        cnt++;
+        DQ.push_back(DQ.front());
+        DQ.pop_front();
+    }  
+    cout << DQ.front() << ">";
 
     return 0;
 }
