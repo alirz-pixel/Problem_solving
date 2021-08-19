a4a106346e1df56f503e484a6961d87653d8a194
alirz-pixel <alirce@naver.com>
2021-08-20 04:53:01 +0900

    백준 14645번 브론즈5

---
+++ baekjoon/14645 와이버스 부릉부릉.cpp
@@ -0,0 +1,34 @@
+/*  2021-08-20  4:49
+백준 14645번 와이버스 부릉부릉 || 브론즈5
+https://www.acmicpc.net/problem/14645
+
+
+
+... 힐링?
+
+시작시간 4:49
+종료시간 4:51       걸린시간 : 2분  ||  시도횟수 : 1회
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout.tie(0);
+
+
+    int n, m, a, b;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+        cin >> a >> b;
+
+    cout << "비와이";
+    return 0;
+}
