ba309b68d84700271a9d662790be97d9e7de16ec
alirz-pixel <alirce@naver.com>
2021-08-22 18:44:27 +0900

    백준 13904번 골드3

--- baekjoon/13904 과제.cpp
+++ baekjoon/13904 과제.cpp
@@ -1,6 +1,6 @@
 /*  2021-08-22  2:27
 백준 13904번 과제 || 골드3
-https://www.acmicpc.net/problem/16234
+https://www.acmicpc.net/problem/13904
 
 
 
@@ -21,54 +21,69 @@ https://www.acmicpc.net/problem/16234
 이렇게 0일까지 반복하면 쉽게 풀 수 있다.
 ===========================
 
-시작시간 2:10
-종료시간 2:15       걸린시간 : 15분  ||  시도횟수 : 1회
-*/
+2차 풀이 방법 :
+ - 1. 과제의 점수가 높은 순으로 정렬한다.
+ - 2. 과제의 점수가 높은 순으로 순회한다. 이 때
+ - 2-1 만약, 현재 순회중인 이 과제의 마감일이 나온적이 없다면 방문처리를 해준 후, result 변수에 더해준다.
+ - 2-2                                     나온적이 있다면 0보다 크고 마감일보다 작은 곳에 방문처리를 해주고 result 변수에 더해준다.
+===============================
 
+
+시작시간 2:10  &&  2:34
+종료시간 2:15  &&  2:43        걸린시간 : 15분  ||  시도횟수 : 1회
+                         2차 : 걸린시간 : 9분  ||  시도횟수 : 1회
+*/
 
 
 #include <algorithm>
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
     cin.tie(0);
-    cout.tie(0);
     ios::sync_with_stdio(false);
+    cout.tie(0);
 
+    
     int n;
     cin >> n;
 
-    priority_queue<int> pq;
-    vector<vector<int>> day(1001);
+    vector<pair<int, int>> input(n);
+    for (int i = 0; i < n; i++)
+        cin >> input[i].second >> input[i].first;
 
-    int inputDay, inputEnd, maxDay = 0;
+    sort(input.begin(), input.end(), greater<pair<int,int>>());
+
+    // solve();
+    vector<bool> day(1001);
+    int j, result = 0;
     for (int i = 0; i < n; i++)
     {
-        cin >> inputDay >> inputEnd;
-
-        maxDay = max(inputDay, maxDay);
-        day[inputDay].push_back(inputEnd);
-    }
-
-
-    int result = 0;    
-    for (int i = maxDay; i > 0; i--)
-    {
-        for (int j = 0; j < day[i].size(); j++)
-            pq.push(day[i][j]);
-
-
-        if (!pq.empty())
+        if (!day[input[i].second])
         {
-            result += pq.top();
-            pq.pop();
+            result += input[i].first;
+            day[input[i].second] = true;
+        }
+
+        else
+        {
+            for (j = input[i].second; j > 0; j--)
+            {
+                if (day[j] == false)
+                    break;
+            }
+
+            if (j != 0)
+            {
+                result += input[i].first;
+                day[j] = true;
+            }
         }
     }
-
+    
     cout << result;
     return 0;
 }
