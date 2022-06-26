58ad88c80f852b39f660bdc7d46c216cb5a7c78a
alirz-pixel <alirce@naver.com>
2022-06-26 17:16:17 +0900

    SW Expert Academy - 암호생성기 (정답률 : 81%)

---
+++ SW Expert Academy/1225. 암호생성기.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <climits>
+#define PASSWDLENGTH 8
+
+using namespace std;
+
+int main() {
+	vector<int> arr(PASSWDLENGTH);
+	
+	for (int tc = 1; tc <= 10; tc++) {
+		int dummy_tc;
+		cin >> dummy_tc;
+
+		for (auto &i : arr) {
+			cin >> i;
+		}
+
+		int head_idx = 0;
+		int cycle = 1;
+		while (arr[head_idx] > 0) {
+			arr[head_idx] -= cycle;
+			cycle = cycle % 5 + 1;
+
+			if (arr[head_idx] <= 0) {
+				arr[head_idx] = 0;
+				break;
+			}
+			head_idx = (head_idx + 1) % PASSWDLENGTH;
+		}
+
+		cout << "#" << tc << " ";
+
+		for (int i = 0; i < PASSWDLENGTH; i++) {
+			head_idx = (head_idx + 1) % PASSWDLENGTH;
+			cout << arr[head_idx] << " ";
+		}
+		cout << "\n";
+	}
+}
